cdf907f11633b1396fc93d7af9a2eaeb13ebcb39
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-11-09 11:16:11 +0000

    Use Sourcy.DotNet.Projects.Sourcy_Tests

--- Sourcy.Pipeline/Modules/RunUnitTestsModule.cs
+++ Sourcy.Pipeline/Modules/RunUnitTestsModule.cs
@@ -10,38 +10,20 @@ using File = ModularPipelines.FileSystem.File;
 
 namespace Sourcy.Pipeline.Modules;
 
-public class RunUnitTestsModule : Module<List<CommandResult>>
+public class RunUnitTestsModule : Module<CommandResult>
 {
-    protected override async Task<List<CommandResult>?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    protected override async Task<CommandResult?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
     {
-        var projectFile = context.Git()
-            .RootDirectory
-            .AssertExists()
-            .FindFile(x => x.Name == "TomLonghurst.Selenium.PlaywrightWebDriver.Tests.csproj")
-            .AssertExists();
-        
-        var results = new List<CommandResult>
-        {
-            await Run(context, cancellationToken, projectFile, true),
-            await Run(context, cancellationToken, projectFile, false)
-        };
-
-        return results;
+        return await Run(context, Sourcy.DotNet.Projects.Sourcy_Tests, cancellationToken);
     }
 
-    private static async Task<CommandResult> Run(IPipelineContext context, CancellationToken cancellationToken,
-        File unitTestProjectFile, bool isV3)
+    private static async Task<CommandResult> Run(IPipelineContext context, File unitTestProjectFile, CancellationToken cancellationToken)
     {
         var dotNetTestOptions = new DotNetTestOptions
         {
             ProjectSolutionDirectoryDllExe = unitTestProjectFile.Path,
             CommandLogging = CommandLogging.Input | CommandLogging.Error,
         };
-
-        if (isV3)
-        {
-            dotNetTestOptions.Properties = new[] { new KeyValue("SeleniumVersion", "3") };
-        }
         
         return await context.DotNet().Test(dotNetTestOptions, cancellationToken);
     }
