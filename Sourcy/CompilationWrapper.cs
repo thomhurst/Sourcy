64d136ee5006021fcc05347dd36a42ae8eee3f0b
Tom Longhurst <130671542+msm-tomlonghurst@users.noreply.github.com>
2024-11-20 16:43:40 +0000

    Tweak

--- Sourcy/CompilationWrapper.cs
+++ Sourcy/CompilationWrapper.cs
@@ -1,19 +1,18 @@
-using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
 
 namespace Sourcy;
 
-internal struct CompilationWrapper(Compilation compilation)
+internal readonly struct CompilationWrapper(Compilation compilation)
 {
     public Compilation Compilation { get; } = compilation;
 
     public override bool Equals(object? obj)
     {
-        return true;
+        return obj is Compilation;
     }
 
     public override int GetHashCode()
     {
-        return 1;
+        return compilation.GetHashCode();
     }
 }
