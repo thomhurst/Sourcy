28c7083fac4a4489e7ebacef70cfc7a043642906
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-18 19:19:42 +0000

    IEqualityComparer<Root>

--- Sourcy/Root.cs
+++ Sourcy/Root.cs
@@ -7,7 +7,7 @@ using System.IO;
 namespace Sourcy;
 
 [DebuggerDisplay("{Directory,nq}")]
-public record Root(DirectoryInfo Directory)
+public record Root(DirectoryInfo Directory) : IEqualityComparer<Root>
 {
     public IEnumerable<FileInfo> EnumerateFiles()
     {
@@ -17,5 +17,35 @@ public record Root(DirectoryInfo Directory)
     public IEnumerable<DirectoryInfo> EnumerateDirectories()
     {
         return SafeWalk.EnumerateDirectories(Directory);
+    }
+
+    public bool Equals(Root? x, Root? y)
+    {
+        if (ReferenceEquals(x, y))
+        {
+            return true;
+        }
+
+        if (x is null)
+        {
+            return false;
+        }
+
+        if (y is null)
+        {
+            return false;
+        }
+
+        if (x.GetType() != y.GetType())
+        {
+            return false;
+        }
+
+        return x.Directory.FullName.Equals(y.Directory.FullName);
+    }
+
+    public int GetHashCode(Root obj)
+    {
+        return obj.Directory.GetHashCode();
     }
 }
