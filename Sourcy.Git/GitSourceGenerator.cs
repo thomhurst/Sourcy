2c6bae949495125af4d6297feb7aeac798bf06e3
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-04 10:55:29 +0000

    Tweak

--- Sourcy.Git/GitSourceGenerator.cs
+++ Sourcy.Git/GitSourceGenerator.cs
@@ -17,21 +17,15 @@ internal class GitSourceGenerator : BaseSourcyGenerator
 
     private static async Task ExecuteAsync(SourceProductionContext context, Root root)
     {
-        await Task.WhenAll(
-            RootDirectory(context, root.Directory.FullName),
-            BranchName(context, root.Directory.FullName)
-            );
+        await RootDirectory(context, root.Directory.FullName);
+        await BranchName(context, root.Directory.FullName);
     }
 
     private static async Task RootDirectory(SourceProductionContext context, string? location)
     {
         var root = await Policy.Handle<Exception>()
-            .OrResult<BufferedCommandResult>(x => string.IsNullOrWhiteSpace(x.StandardOutput))
             .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(i))
-            .ExecuteAsync(async () => await Cli.Wrap("git")
-                .WithArguments(["rev-parse", "--show-toplevel"])
-                .WithWorkingDirectory(location!)
-                .ExecuteBufferedAsync());
+            .ExecuteAsync(async () => await GetGitOutput(location!, ["rev-parse", "--show-toplevel"]));
 
         context.AddSource("GitRootExtensions.g.cs", GetSourceText(
             $$"""
@@ -39,34 +33,49 @@ internal class GitSourceGenerator : BaseSourcyGenerator
 
               internal static partial class Git
               {
-                  public static global::System.IO.DirectoryInfo RootDirectory { get; } = new global::System.IO.DirectoryInfo("{{root.StandardOutput.Trim()}}");
+                  public static global::System.IO.DirectoryInfo RootDirectory { get; } = new global::System.IO.DirectoryInfo("{{root}}");
               }
               """
         ));
     }
-    
+
     private static async Task BranchName(SourceProductionContext context, string? location)
     {
         var branch = await Policy.Handle<Exception>()
-            .OrResult<BufferedCommandResult>(x => string.IsNullOrWhiteSpace(x.StandardOutput))
             .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(i))
-            .ExecuteAsync(async () => await Cli.Wrap("git")
-                .WithArguments(["rev-parse", "--abbrev-ref", "HEAD"])
-                .WithWorkingDirectory(location!)
-                .ExecuteBufferedAsync());
+            .ExecuteAsync(async () => await GetGitOutput(location!, ["rev-parse", "--abbrev-ref", "HEAD"]));
 
-        if (!string.IsNullOrWhiteSpace(branch.StandardOutput))
+        context.AddSource("GitBranchNameExtensions.g.cs", GetSourceText(
+            $$"""
+              namespace Sourcy;
+
+              internal static partial class Git
+              {
+                  public static string BranchName { get; } = "{{branch}}";
+              }
+              """
+        ));
+    }
+
+    private static async Task<string> GetGitOutput(string location, string[] args)
+    {
+        var bufferedCommandResult = await Cli.Wrap("git")
+            .WithArguments(args)
+            .WithWorkingDirectory(location!)
+            .ExecuteBufferedAsync();
+
+        var output = bufferedCommandResult.StandardOutput.Trim();
+
+        if (string.IsNullOrEmpty(output))
         {
-            context.AddSource("GitBranchNameExtensions.g.cs", GetSourceText(
-                $$"""
-                  namespace Sourcy;
-
-                  internal static partial class Git
-                  {
-                      public static string BranchName { get; } = "{{branch.StandardOutput.Trim()}}";
-                  }
-                  """
-            ));
+            throw new Exception($"git {string.Join(" ", args)} returned no output.");
         }
+        
+        if(!string.IsNullOrWhiteSpace(bufferedCommandResult.StandardError))
+        {
+            throw new Exception($"git {string.Join(" ", args)} returned an error: {bufferedCommandResult.StandardError}");
+        }
+        
+        return output;
     }
 }
